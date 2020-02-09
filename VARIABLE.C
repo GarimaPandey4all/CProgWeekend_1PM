a2ed17ebc3728fdd52b19ba5c974e42ce809964b
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2020-02-09 01:16:28 -0800

    Add files via upload

---
+++ VARIABLE.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+//#define PI 3.14
+
+void main()
+{
+	const int a=10;
+	int b=30,c;
+	clrscr();
+
+	a = 20;
+
+	c = a + b;
+
+	printf("Sum of Two Numbers is:%d", c);
+
+	getch();
+
+}
